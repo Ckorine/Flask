1018c6eedb4e63c63c56cfd7c2cf64c819df6f40
Stellabenja <benjamine1994@gmail.com>
2020-06-19 10:50:01 +0200

    delete unused dirctory and fix register form

--- User.py
+++ User.py
@@ -1,4 +1,7 @@
 from mongoengine import *
+from wtforms.fields.html5 import DateField
+
+
 class User(Document):
     firstname= StringField(required=True, max_length=30)
     lastname = StringField(required=True, )
